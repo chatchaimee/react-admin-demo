c966e9a65bb255ae15634f709e26c0cdbbe2526e
chatchaimee <chatchai.meesuksabai@hotmail.com>
2018-09-17 14:59:55 +0700

    Prepare dataProvider for custom fetch api

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
-import { Title, Responsive } from 'react-admin';
+import { Title, Responsive, fetchEnd, fetchStart, GET_ONE } from 'react-admin';
 import { Card, CardContent, Typography } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { Line, Bar, Pie } from 'react-chartjs-2';
+import { customDataProvider } from '../../dataProvider';
 import config from '../../config';
 
 const { title } = config;
@@ -62,7 +63,7 @@ const RenderChart = props => {
 
 class ChartComponent extends Component {
 	state = {
-		data: []
+		data: {}
 	};
 
 	componentDidMount() {
@@ -76,71 +77,104 @@ class ChartComponent extends Component {
 	}
 
 	getData = () => {
-		// TODO: fetch data from api
-		let data = [];
+		const { fetchStart, fetchEnd } = this.props;
+		const params = { id: 1 };
 
-		switch (this.props.type) {
-			case 'line':
-				data = {
-					labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-					datasets: [
-						{
-							label: 'Data',
-							fill: false,
-							lineTension: 0.1,
-							backgroundColor: 'rgba(75,192,192,0.4)',
-							borderColor: 'rgba(75,192,192,1)',
-							borderCapStyle: 'butt',
-							borderDash: [],
-							borderDashOffset: 0.0,
-							borderJoinStyle: 'miter',
-							pointBorderColor: 'rgba(75,192,192,1)',
-							pointBackgroundColor: '#fff',
-							pointBorderWidth: 1,
-							pointHoverRadius: 5,
-							pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-							pointHoverBorderColor: 'rgba(220,220,220,1)',
-							pointHoverBorderWidth: 2,
-							pointRadius: 1,
-							pointHitRadius: 10,
-							data: [65, 59, 80, 81, 56, 55, 40]
-						}
-					]
-				};
-				break;
-			case 'bar':
-				data = {
-					labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-					datasets: [
-						{
-							label: 'My First dataset',
-							backgroundColor: 'rgba(255,99,132,0.2)',
-							borderColor: 'rgba(255,99,132,1)',
-							borderWidth: 1,
-							hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-							hoverBorderColor: 'rgba(255,99,132,1)',
-							data: [65, 59, 80, 81, 56, 55, 40]
-						}
-					]
-				};
-				break;
-			case 'pie':
-				data = {
-					labels: ['Red', 'Green', 'Yellow'],
-					datasets: [
-						{
-							data: [300, 50, 100],
-							backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-							hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-						}
-					]
-				};
-				break;
-			default:
-				data = [];
-		}
+		// Dispatch an action letting react-admin know a API call is ongoing
+		fetchStart();
 
-		this.setState({ data: data });
+		customDataProvider(GET_ONE, 'posts', params)
+			.then(res => {
+				let data = {};
+
+				console.log(res);
+
+				switch (this.props.type) {
+					case 'line':
+						data = {
+							labels: [
+								'January',
+								'February',
+								'March',
+								'April',
+								'May',
+								'June',
+								'July'
+							],
+							datasets: [
+								{
+									label: 'Data',
+									fill: false,
+									lineTension: 0.1,
+									backgroundColor: 'rgba(75,192,192,0.4)',
+									borderColor: 'rgba(75,192,192,1)',
+									borderCapStyle: 'butt',
+									borderDash: [],
+									borderDashOffset: 0.0,
+									borderJoinStyle: 'miter',
+									pointBorderColor: 'rgba(75,192,192,1)',
+									pointBackgroundColor: '#fff',
+									pointBorderWidth: 1,
+									pointHoverRadius: 5,
+									pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+									pointHoverBorderColor: 'rgba(220,220,220,1)',
+									pointHoverBorderWidth: 2,
+									pointRadius: 1,
+									pointHitRadius: 10,
+									data: [65, 59, 80, 81, 56, 55, 40]
+								}
+							]
+						};
+						break;
+					case 'bar':
+						data = {
+							labels: [
+								'January',
+								'February',
+								'March',
+								'April',
+								'May',
+								'June',
+								'July'
+							],
+							datasets: [
+								{
+									label: 'My First dataset',
+									backgroundColor: 'rgba(255,99,132,0.2)',
+									borderColor: 'rgba(255,99,132,1)',
+									borderWidth: 1,
+									hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+									hoverBorderColor: 'rgba(255,99,132,1)',
+									data: [65, 59, 80, 81, 56, 55, 40]
+								}
+							]
+						};
+						break;
+					case 'pie':
+						data = {
+							labels: ['Red', 'Green', 'Yellow'],
+							datasets: [
+								{
+									data: [300, 50, 100],
+									backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+									hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+								}
+							]
+						};
+						break;
+					default:
+						data = {};
+				}
+
+				this.setState({ data: data });
+			})
+			.catch(error => {
+				console.log(error);
+			})
+			.finally(() => {
+				// Dispatch an action letting react-admin know a API call has ended
+				fetchEnd();
+			});
 	};
 
 	render() {
@@ -160,4 +194,9 @@ const mapStateToProps = state => ({
 	views: state.admin.ui.viewVersion
 });
 
-export const Chart = connect(mapStateToProps, {})(ChartComponent);
+const mapDispatchToProps = {
+	fetchEnd,
+	fetchStart
+};
+
+export const Chart = connect(mapStateToProps, mapDispatchToProps)(ChartComponent);
