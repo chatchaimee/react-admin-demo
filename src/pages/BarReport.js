803b2b900b8e85d5544bb4550817c3f1f41b4e27
chatchaimee <chatchai.meesuksabai@hotmail.com>
2018-09-20 12:04:27 +0700

    Remove bar option

--- src/pages/BarReport.js
+++ src/pages/BarReport.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Chart } from '../components';
 
-const options = {
-	maintainAspectRatio: false
-};
+const options = {};
 
 export const BarReport = props => {
 	return <Chart title="Bar Chart" type="bar" options={options} />;
